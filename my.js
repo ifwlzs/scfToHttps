533a3b72524c95321b9e53cb0a6f232d7ba4aae5
ifwlzs <49548316+ifwlzs@users.noreply.github.com>
2020-04-15 18:40:31 +0800

    Add files via upload

---
+++ my.js
@@ -0,0 +1,7 @@
+(function() {
+    var link = document.createElement('link');
+    link.type = 'image/x-icon';
+    link.rel = 'shortcut icon';
+    link.href = 'https://cdn.jsdelivr.net/gh/ifwlzs/ifwlzs.github.io/medias/favicon.ico';
+    document.getElementsByTagName('head')[0].appendChild(link);
+}());
